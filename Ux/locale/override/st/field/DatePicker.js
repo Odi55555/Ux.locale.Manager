6dc6a80bc82dcadb98ceaa69e9a745fc701d9b35
Armin Odenkirchen <aowerb@gmail.com>
2014-02-14 11:05:42 +0100

    FIX datepickerfield dateFormat override for ST 2.3

--- Ux/locale/override/st/field/DatePicker.js
+++ Ux/locale/override/st/field/DatePicker.js
@@ -1,6 +1,10 @@
 Ext.define('Ux.locale.override.st.field.DatePicker', {
     override : 'Ext.field.DatePicker',
 
+    requires : [
+        'Ux.locale.override.st.Component'
+    ],
+
     getPicker : function() {
         var picker     = this._picker,
             needLocale = picker && !picker.isPicker;
@@ -12,5 +16,28 @@ Ext.define('Ux.locale.override.st.field.DatePicker', {
         }
 
         return picker;
+    },
+
+    setLocale : function(locale) {
+        var me                 = this,
+            locales            = me.locales || me.getInitialConfig().locales,
+            dateFormat         = locales.dateFormat,
+            manager            = me.locale,
+            defaultDateFormat  = Ext.util.Format.defaultDateFormat;
+
+        if (dateFormat) {
+            if (Ext.isObject(dateFormat)) {
+                defaultDateFormat = dateFormat.defaultDateFormat;
+                dateFormat        = dateFormat.key;
+            }
+
+            dateFormat = manager.get(dateFormat, defaultDateFormat);
+
+            if (Ext.isString(dateFormat)) {
+                me.setDateFormat(dateFormat);
+            }
+        }
+
+        me.callParent(arguments);
     }
 });
